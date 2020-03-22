93cda635beeede8c5af407b9038b0fc5a4a2f7b4
Dustin LaDue <dustin.ladue@yahoo.com>
2020-03-22 17:16:31 -0600

    Add code

---
+++ src/part.js
@@ -0,0 +1,34 @@
+class Part{
+  constructor({name, type, value}) {
+    this.name = name,
+    this.validTypes = [
+    'shell',
+    'hyperdrive',
+    'computer',
+    'life support',
+    'landing gear',
+    undefined
+  ],
+    this.type = this.validTypes.includes(type) ? type : undefined,
+    this.value = value,
+    this.broken = false;
+
+  }
+ isValid(name, type, value) {
+   if(this.name === undefined) {
+     return false;
+   }
+   if(this.type === undefined) {
+     return false;
+   }
+  if(this.value === undefined) {
+    return false;
+   }
+   else {
+     return true;
+   }
+  }
+}
+
+
+module.exports = Part;
